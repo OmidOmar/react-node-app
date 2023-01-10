eb4d8d34697adb5a6a59d3fa92b3aa0719b540ec
OmidOmar <a.omidomari@gmail.com>
2023-01-10 15:40:17 +0000

    final commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+
+const port = process.env.PORT || 3001;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Server!" });
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
